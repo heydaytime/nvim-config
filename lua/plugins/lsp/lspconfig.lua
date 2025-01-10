58b6cded458dc91ceea0490a60e5b773e253f02f
heydaytime <whynotmihir@gmail.com>
2025-01-10 15:37:43 +0530

    Fixed dartls crashing when pasting text (Vgg or VGG then p)

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -16,89 +16,77 @@ return {
     },
   },
   config = function()
-    -- import lspconfig plugin
+    -- Import required plugins
     local lspconfig = require("lspconfig")
-
-    -- import mason_lspconfig plugin
     local mason_lspconfig = require("mason-lspconfig")
-
-    -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local cmp = require("cmp")
-
-    -- Setup completion keybinds
-    cmp.setup({
-      mapping = {
-        -- Accept the highlighted completion item with Tab
-        ["<Tab>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }),
-      },
-    })
-
-    local keymap = vim.keymap -- for conciseness
-
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      callback = function(ev)
-        local opts = { buffer = ev.buf, silent = true }
-
-        -- set keybinds
-        opts.desc = "Show LSP references"
-        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-        opts.desc = "Go to declaration"
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-        opts.desc = "Show LSP implementations"
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-        opts.desc = "Show LSP type definitions"
-        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-        opts.desc = "See available code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-        opts.desc = "Smart rename"
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-        opts.desc = "Show line diagnostics"
-        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-        opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-        opts.desc = "Show documentation for what is under cursor"
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        opts.desc = "Restart LSP"
-        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-
-        -- Map <leader>rf to open Telescope Flutter commands
-        opts.desc = "Telescope: Flutter Commands"
-        keymap.set("n", "<leader>rf", function()
-          require("telescope").extensions.flutter.commands()
-        end, opts)
-      end,
-    })
-
-    -- used to enable autocompletion (assign to every lsp server config)
+    -- Setup completion capabilities
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- Change the Diagnostic symbols in the sign column (gutter)
+    -- Setup diagnostic symbols
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    mason_lspconfig.setup_handlers({
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
+    -- LSP keybindings
+    local keymap = vim.keymap -- for conciseness
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf, silent = true }
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
       end,
+    })
 
+    -- Setup Mason and generic LSP server handling
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        if server_name ~= "dartls" then -- Avoid direct setup for dartls
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end
+      end,
+    })
+
+    -- Reload DartLS if inactive
+    local function reload_dartls_if_inactive()
+      local dartls_client
+      for _, client in ipairs(vim.lsp.get_clients()) do
+        if client.name == "dartls" then
+          dartls_client = client
+          break
+        end
+      end
+
+      vim.defer_fn(function()
+        if dartls_client and not dartls_client.is_stopped() then
+          return
+        end
+
+        if dartls_client and dartls_client.stop then
+          dartls_client.stop()
+        end
+
+        require("flutter-tools.lsp").attach()
+      end, 2000)
+    end
+
+    -- Auto-reload DartLS on saving Dart files
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = "*.dart",
+      callback = reload_dartls_if_inactive,
     })
   end,
 }
