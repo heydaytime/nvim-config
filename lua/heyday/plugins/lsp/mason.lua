02ae51c448334fe2e48ea74e027cbe5fdf6b350a
heydaytime <whynotmihir@gmail.com>
2025-03-30 22:00:26 -0500

    Spent 7 hours removing a lsp digonistic bug for clang

--- lua/heyday/plugins/lsp/mason.lua
+++ lua/heyday/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "grammarly",
+        "clangd",
         "jdtls",
         "ts_ls",
         "html",
