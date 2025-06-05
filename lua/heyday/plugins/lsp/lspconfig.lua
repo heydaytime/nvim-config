c2837a9d407d514016413b20f0d040226c178e55
heydaytime <whynotmihir@gmail.com>
2025-06-05 06:19:19 +0530

    Working on a fix... Dual lsps? wth?

--- lua/heyday/plugins/lsp/lspconfig.lua
+++ lua/heyday/plugins/lsp/lspconfig.lua
@@ -8,97 +8,84 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
-    local mason_lspconfig = require("mason-lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     vim.diagnostic.config({
       signs = {
         text = {
-          [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
           [vim.diagnostic.severity.HINT] = "󰠠 ",
-          [vim.diagnostic.severity.INFO] = " ",
+          [vim.diagnostic.severity.INFO] = " ",
         },
       },
     })
 
-    mason_lspconfig.setup({
-      handlers = {
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-
-        ["clangd"] = function()
-          lspconfig["clangd"].setup({
-            capabilities = capabilities,
-            cmd = {
-              "clangd",
-              "--query-driver=/Users/mihirbelose/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc",
-            },
-          })
-        end,
-
-        ["jdtls"] = function()
-          lspconfig["jdtls"].setup({
-            capabilities = capabilities,
-            settings = {
-              java = {
-                project = {
-                  referencedLibraries = {
-                    "/Users/mihirbelose/ProgrammingProjects/SchoolProjects/JavaProjects/junit5.jar",
-                  },
-                },
-              },
-            },
-          })
-        end,
-
-        ["svelte"] = function()
-          lspconfig["svelte"].setup({
-            capabilities = capabilities,
-            filetypes = { "typescript", "javascript", "svelte", "html", "css" },
-            on_attach = function(client, bufnr)
-              vim.api.nvim_create_autocmd("BufWritePost", {
-                pattern = { "*.js", "*.ts" },
-                buffer = bufnr,
-                callback = function(ctx)
-                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-                end,
-              })
-            end,
-          })
-        end,
-
-        ["graphql"] = function()
-          lspconfig["graphql"].setup({
-            capabilities = capabilities,
-            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-          })
-        end,
-
-        ["emmet_ls"] = function()
-          lspconfig["emmet_ls"].setup({
-            capabilities = capabilities,
-            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-          })
-        end,
-
-        ["lua_ls"] = function()
-          lspconfig["lua_ls"].setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = { globals = { "vim" } },
-                completion = { callSnippet = "Replace" },
-              },
-            },
-          })
-        end,
+    -- Configure servers directly with lspconfig
+    -- Custom configurations
+    lspconfig.clangd.setup({
+      capabilities = capabilities,
+      cmd = {
+        "clangd",
+        "--query-driver=/Users/mihirbelose/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc",
       },
     })
+
+    lspconfig.jdtls.setup({
+      capabilities = capabilities,
+      settings = {
+        java = {
+          project = {
+            referencedLibraries = {
+              "/Users/mihirbelose/ProgrammingProjects/SchoolProjects/JavaProjects/junit5.jar",
+            },
+          },
+        },
+      },
+    })
+
+    lspconfig.svelte.setup({
+      capabilities = capabilities,
+      filetypes = { "typescript", "javascript", "svelte", "html", "css" },
+      on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = { "*.js", "*.ts" },
+          buffer = bufnr,
+          callback = function(ctx)
+            client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+          end,
+        })
+      end,
+    })
+
+    lspconfig.graphql.setup({
+      capabilities = capabilities,
+      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+    })
+
+    lspconfig.emmet_ls.setup({
+      capabilities = capabilities,
+      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    })
+
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = { globals = { "vim" } },
+          completion = { callSnippet = "Replace" },
+        },
+      },
+    })
+
+    -- Default configurations for other servers
+    local default_servers = { "gopls", "ts_ls", "html", "cssls", "tailwindcss", "prismals", "pyright" }
+
+    for _, server in ipairs(default_servers) do
+      lspconfig[server].setup({
+        capabilities = capabilities,
+      })
+    end
   end,
 }
