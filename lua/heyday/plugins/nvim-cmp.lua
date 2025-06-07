578abff65743429f61569094c864ab077267339e
heydaytime <whynotmihir@gmail.com>
2025-06-07 13:01:34 +0530

    Don't even remember what I changed

--- lua/heyday/plugins/nvim-cmp.lua
+++ lua/heyday/plugins/nvim-cmp.lua
@@ -31,6 +31,7 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
+
       completion = { completeopt = "menu,menuone,noinsert" },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item(),
@@ -39,16 +40,16 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete({}),
-        ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { "i", "s" }),
+        -- ["<C-l>"] = cmp.mapping(function()
+        --   if luasnip.expand_or_locally_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   end
+        -- end, { "i", "s" }),
+        -- ["<C-h>"] = cmp.mapping(function()
+        --   if luasnip.locally_jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   end
+        -- end, { "i", "s" }),
       }),
       sources = {
         { name = "lazydev", group_index = 0 },
