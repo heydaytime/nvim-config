5a5072c3a1d74127f33ff3980c1c886958f49a6c
heydaytime <whynotmihir@gmail.com>
2025-08-20 12:42:01 +0530

    harpoon is shift irrelevant

--- lua/heyday/plugins/harpoon.lua
+++ lua/heyday/plugins/harpoon.lua
@@ -18,20 +18,61 @@ return {
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "Open harpoon menu" })
 
+    -- File selection using number keys:
+    vim.keymap.set("n", "<leader>1", function()
+      harpoon:list():select(1)
+    end, { desc = "Go to harpoon file 1" })
+
+    vim.keymap.set("n", "<leader>2", function()
+      harpoon:list():select(2)
+    end, { desc = "Go to harpoon file 2" })
+
+    vim.keymap.set("n", "<leader>3", function()
+      harpoon:list():select(3)
+    end, { desc = "Go to harpoon file 3" })
+
+    vim.keymap.set("n", "<leader>4", function()
+      harpoon:list():select(4)
+    end, { desc = "Go to harpoon file 4" })
+
+    vim.keymap.set("n", "<leader>5", function()
+      harpoon:list():select(5)
+    end, { desc = "Go to harpoon file 5" })
+
+    vim.keymap.set("n", "<leader>6", function()
+      harpoon:list():select(6)
+    end, { desc = "Go to harpoon file 6" })
+
+    vim.keymap.set("n", "<leader>7", function()
+      harpoon:list():select(7)
+    end, { desc = "Go to harpoon file 7" })
+
+    vim.keymap.set("n", "<leader>8", function()
+      harpoon:list():select(8)
+    end, { desc = "Go to harpoon file 8" })
+
+    vim.keymap.set("n", "<leader>9", function()
+      harpoon:list():select(9)
+    end, { desc = "Go to harpoon file 9" })
+
+    vim.keymap.set("n", "<leader>0", function()
+      harpoon:list():select(10)
+    end, { desc = "Go to harpoon file 10" })
+
     -- File selection using shifted keys:
     vim.keymap.set("n", "<leader>!", function()
       harpoon:list():select(1)
     end, { desc = "Go to harpoon file 1" })
 
-    vim.keymap.set("n", "<leader>@", function()
+    vim.keymap.set("n", "<leader>[", function()
       harpoon:list():select(2)
     end, { desc = "Go to harpoon file 2" })
 
-    vim.keymap.set("n", "<leader>#", function()
+    vim.keymap.set("n", "<leader>{", function()
       harpoon:list():select(3)
     end, { desc = "Go to harpoon file 3" })
 
-    vim.keymap.set("n", "<leader>$", function()
+    vim.keymap.set("n", "<leader>(", function()
       harpoon:list():select(4)
     end, { desc = "Go to harpoon file 4" })
 
@@ -39,7 +80,7 @@ return {
       harpoon:list():select(5)
     end, { desc = "Go to harpoon file 5" })
 
-    vim.keymap.set("n", "<leader>^", function()
+    vim.keymap.set("n", "<leader>*", function()
       harpoon:list():select(6)
     end, { desc = "Go to harpoon file 6" })
 
@@ -47,15 +88,15 @@ return {
       harpoon:list():select(7)
     end, { desc = "Go to harpoon file 7" })
 
-    vim.keymap.set("n", "<leader>*", function()
+    vim.keymap.set("n", "<leader>)", function()
       harpoon:list():select(8)
     end, { desc = "Go to harpoon file 8" })
 
-    vim.keymap.set("n", "<leader>(", function()
+    vim.keymap.set("n", "<leader>]", function()
       harpoon:list():select(9)
     end, { desc = "Go to harpoon file 9" })
 
-    vim.keymap.set("n", "<leader>)", function()
+    vim.keymap.set("n", "<leader>}", function()
       harpoon:list():select(10)
     end, { desc = "Go to harpoon file 10" })
   end,
